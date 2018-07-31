eb4a2c6e6e20689a197d9eaebf796289b62b2e12
Austin Crane <austincrane@gmail.com>
2018-07-31 15:08:55 -0400

    first swing

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The header for theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+<script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+  <section class="header10 cid-qWEInRfqx9 mbr-fullscreen mbr-parallax-background" id="header10-9">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-column mbr-white col-lg-8 col-md-10 ml-auto">
+            
+            
+            
+            
+        </div>
+    </div>
+
+    
+</section>
---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * The header for theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+<script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+
+
+<?php wp_head(); 
+
+$wp_query = new WP_Query(array('post_status'=>'private','pagename'=>'homepage'));
+if ( have_posts() ) : the_post(); 
+
+	$header_image = get_field('header_image');
+	$logo = get_field('logo');
+	$main_title = get_field('main_title');
+	$sub_taglines = get_field('sub_taglines');
+
+	$logo_ggm = get_field('logo_ggm');
+	$background_image_ggm = get_field('background_image_ggm');
+	$info_ggm = get_field('info_ggm');
+
+	$logo_nhor = get_field('logo_nhor');
+	$background_image_nhor = get_field('background_image_nhor');
+	$info_nhor = get_field('info_nhor');
+
+	$logo_gt = get_field('logo_gt');
+	$background_image_gt = get_field('background_image_gt');
+	$info_gt = get_field('info_gt');
+
+	$logo_hot = get_field('logo_hot');
+	$background_image_hot = get_field('background_image_hot');
+	$info_hot = get_field('info_hot');
+
+	$title = get_field('title');
+	$description = get_field('description');
+	$footer_background_photo = get_field('footer_background_photo');
+
+	// echo '<pre>';
+	// print_r($logo_ggm);
+	// echo '</pre>';
+
+endif;
+
+			?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<section class="header parallax-window" data-parallax="scroll" data-image-src="<?php echo $header_image['url']; ?>">
+	<div class="logo">
+		<img src="<?php echo $logo['url']; ?>">
+	</div>
+	<div class="sitedesc">
+		<h1><?php echo $main_title; ?></h1>
+		<div class="taglines">
+			<?php echo $sub_taglines; ?>
+		</div>
+	</div>
+</section>
+
+<section class="companies">
+	<div class="ggm company">
+		<div class="img">
+			<img src="<?php echo $background_image_ggm['url']; ?>" alt="<?php echo $background_image_ggm['alt']; ?>">
+		</div>
+		<div class="desc">
+			<?php echo $info_ggm; ?>
+		</div>
+		<div class="overlay">
+			<div class="logo">
+				<img src="<?php echo $logo_ggm['url']; ?>" alt="<?php echo $logo_ggm['alt']; ?>">
+			</div>
+		</div>
+	</div>
+	<div class="ggm company">
+		<div class="img">
+			<img src="<?php echo $background_image_nhor['url']; ?>" alt="<?php echo $background_image_nhor['alt']; ?>">
+		</div>
+		<div class="desc">
+			<?php echo $info_nhor; ?>
+		</div>
+		<div class="overlay">
+			<div class="logo">
+				<img src="<?php echo $logo_nhor['url']; ?>" alt="<?php echo $logo_nhor['alt']; ?>">
+			</div>
+		</div>
+	</div>
+	<div class="ggm company">
+		<div class="img">
+			<img src="<?php echo $background_image_gt['url']; ?>" alt="<?php echo $background_image_gt['alt']; ?>">
+		</div>
+		<div class="desc">
+			<?php echo $info_gt; ?>
+		</div>
+		<div class="overlay">
+			<div class="logo">
+				<img src="<?php echo $logo_gt['url']; ?>" alt="<?php echo $logo_gt['alt']; ?>">
+			</div>
+		</div>
+	</div>
+	<div class="ggm company">
+		<div class="img">
+			<img src="<?php echo $background_image_hot['url']; ?>" alt="<?php echo $background_image_hot['alt']; ?>">
+		</div>
+		<div class="desc">
+			<?php echo $info_hot; ?>
+		</div>
+		<div class="overlay">
+			<div class="logo">
+				<img src="<?php echo $logo_hot['url']; ?>" alt="<?php echo $logo_hot['alt']; ?>">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="footer parallax-window" data-parallax="scroll" data-image-src="<?php echo $footer_background_photo['url']; ?>">
+	<div class="logo-e">
+		<img src="<?php //echo  ?>">
+	</div>
+
+	<div class="footer-desc">
+		<h3><?php echo $title; ?></h3>
+		<?php echo $description; ?>
+	</div>
+
+</section>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
